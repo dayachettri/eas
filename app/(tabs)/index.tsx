761538f533a51f72b09e1f6f7e26bd3cf8fcd561
daya chettri <dayasagar073@gmail.com>
2024-02-23 09:15:23 +0530

    update app icon & splash screen

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,15 +1,13 @@
 import { StyleSheet } from 'react-native';
 
-import EditScreenInfo from '@/components/EditScreenInfo';
-import { Text, View } from '@/components/Themed';
+import { WebView } from 'react-native-webview';
 
 export default function TabOneScreen() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
-    </View>
+    <WebView
+      style={styles.container}
+      source={{ uri: 'https://www.google.com' }}
+    />
   );
 }
 
